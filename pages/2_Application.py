2fabe97bbcb947bd0822470d367fba1c08c266cb
Victor Pietono <pietonov@msu.com>
2024-12-10 15:40:20 -0500

    Signed-off-by: Victor Pietono <pietonov@msu.com>

---
+++ pages/2_Application.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Header Section
+st.title("Ground Time Predictor")
+st.image(
+    "DATA/GroundTimePredictor.png", 
+    caption="AI-generated: Streamlining Ground Operations for Better Efficiency", 
+    width=800
+)
