dfae6ecd6654d62f60e490ffa01fc9cff0e33879
Victor Pietono <pietonov@msu.com>
2024-12-10 15:53:54 -0500

    Signed-off-by: Victor Pietono <pietonov@msu.com>

--- Homepage.py
+++ Homepage.py
@@ -1,8 +1,5 @@
 ######################### Import ##############################
 import streamlit as st
-import joblib
-import os
-
 
 
 st.set_page_config(page_title="Movie Ratings Predictor")
@@ -14,23 +11,6 @@ st.image(
     caption="AI-generated: Predicting Movies Ratings before You Watched It", 
     width=800
 )
-
-
-######################### Load Models #########################
-@st.cache_resource
-def load_models():
-    try:
-        glm_path = 'OUTPUT/glm_model.pkl'
-        if not os.path.exists(glm_path):
-            raise FileNotFoundError("Model files not found.")
-        glm_model = joblib.load(glm_path)
-    except Exception as e:
-        st.error(f"Error loading models: {e}")
-        raise
-    return glm_model
-
-glm_full = load_models()
-
 
 
 ######################### Sidebar Navigation ##################
--- pages/2_Application.py
+++ pages/2_Application.py
@@ -1,9 +1,85 @@
 import streamlit as st
+import os
+import joblib
+import pandas as pd
 
 # Header Section
 st.title("Ground Time Predictor")
-st.image(
-    "DATA/GroundTimePredictor.png", 
-    caption="AI-generated: Streamlining Ground Operations for Better Efficiency", 
-    width=800
+
+
+
+
+
+######################### Load Models #########################
+@st.cache_resource
+def load_models():
+    try:
+        glm_path = 'OUTPUT/glm_model.pkl'
+        if not os.path.exists(glm_path):
+            raise FileNotFoundError("Model files not found.")
+        glm_model = joblib.load(glm_path)
+    except Exception as e:
+        st.error(f"Error loading models: {e}")
+        raise
+    return glm_model
+
+glm_full = load_models()
+
+actor_stats = pd.read_csv('OUTPUT/actor_stats.csv')
+director_stats = pd.read_csv('OUTPUT/director_stats.csv')
+
+actors_list = actor_stats['actors_list'].unique().tolist()
+directors_list = director_stats['directors_list'].unique().tolist()
+
+######################### GLM Model #########################
+st.title("Movie Ratings Predictor")
+
+st.sidebar.header("Input Features")
+
+# Select actors
+selected_actors = st.sidebar.multiselect(
+    "Select Actors",
+    options=actors_list,
+    default=None
 )
+
+# Select directors
+selected_directors = st.sidebar.multiselect(
+    "Select Directors",
+    options=directors_list,
+    default=None
+)
+
+# Display user inputs
+st.write("Selected Actors:", selected_actors)
+st.write("Selected Directors:", selected_directors)
+
+
+# # User Input Form
+# with st.form("prediction_form"):
+#     st.header("Input Flight Details")
+#     distance = st.number_input("Distance (miles):", min_value=0, value=500)
+#     large_airport = st.selectbox("Large Airport:", [1, 0], format_func=lambda x: "Yes" if x else "No")
+#     has_passengers = st.selectbox("Has Passengers:", [1, 0], format_func=lambda x: "Yes" if x else "No")
+#     passengers = st.number_input("Number of Passengers:", min_value=0, value=150, disabled=not has_passengers)
+#     is_winter = st.selectbox("Winter Season:", [1, 0], format_func=lambda x: "Yes" if x else "No")
+#     unique_carrier = st.selectbox("Carrier:", unique_carriers)
+#     submitted = st.form_submit_button("Predict")
+
+# if submitted:
+#     input_data = {
+#         'DISTANCE': [distance],
+#         'LARGE_AIRPORT': [large_airport],
+#         'HAS_PASSENGERS': [has_passengers],
+#         'PASSENGERS': [0 if not has_passengers else passengers],
+#         'IS_WINTER': [is_winter],
+#         'UNIQUE_CARRIER': [unique_carrier]
+#     }
+#     input_df = pd.DataFrame(input_data)
+
+#     try:
+#         glm_pred = glm_full.predict(input_df)
+#         st.success(f"GLM Predicted Ground Time: {glm_pred.iloc[0]:.2f} minutes")
+#     except Exception as e:
+#         st.error(f"Error during GLM prediction: {e}")
+
